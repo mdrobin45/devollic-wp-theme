b378214b1dd731149158c644d48712ef553b8d70
mdrobin45 <robin4500bd@gmail.com>
2024-06-15 15:33:47 +0600

    unhooked woocommerce

--- functions.php
+++ functions.php
@@ -177,3 +177,19 @@ if ( defined( 'JETPACK__VERSION' ) ) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
 
+// remove woocommerce hook
+remove_action( 'woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10);
+remove_action( 'woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10);
+
+add_action('woocommerce_before_main_content', 'my_theme_wrapper_start', 10);
+add_action('woocommerce_after_main_content', 'my_theme_wrapper_end', 10);
+
+function my_theme_wrapper_start() {
+    echo '<section id="product-main">';
+}
+
+function my_theme_wrapper_end() {
+    echo '</section>';
+}
+
+	
