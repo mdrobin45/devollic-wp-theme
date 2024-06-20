2a0f0027d221d5613458d678b9ddf05f1418b4c5
mdrobin45 <robin4500bd@gmail.com>
2024-06-20 10:05:38 +0600

    function edited

--- functions.php
+++ functions.php
@@ -99,6 +99,9 @@ function devollic_setup() {
 			'flex-height' => true,
 		)
 	);
+
+   // Woocommerce Support
+   add_theme_support( 'woocommerce' );
 }
 add_action( 'after_setup_theme', 'devollic_setup' );
 
@@ -177,19 +180,5 @@ if ( defined( 'JETPACK__VERSION' ) ) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
 
-// remove woocommerce hook
-remove_action( 'woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10);
-remove_action( 'woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10);
-
-add_action('woocommerce_before_main_content', 'my_theme_wrapper_start', 10);
-add_action('woocommerce_after_main_content', 'my_theme_wrapper_end', 10);
-
-function my_theme_wrapper_start() {
-    echo '<section id="product-main">';
-}
-
-function my_theme_wrapper_end() {
-    echo '</section>';
-}
 
 	
