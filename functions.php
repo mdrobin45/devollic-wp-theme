c5824c697ad6762d35f483f3b09e26ab72c41cd6
mdrobin45 <robin4500bd@gmail.com>
2024-06-20 21:49:26 +0600

    modified:   assets/css/product-archive.css 	modified:   functions.php 	modified:   inc/woocommerce/woocommerce-functions.php

--- functions.php
+++ functions.php
@@ -190,16 +190,6 @@ remove_action('woocommerce_before_shop_loop','woocommerce_catalog_ordering',30);
 
 
 // Path: inc/woocommerce/woocommerce-functions.php
-// if(class_exists('WooCommerce')){
-//    require get_template_directory().'/inc/woocommerce/woocommerce-functions.php';
-// }
-
-// Remove default WooCommerce product title function
-remove_action('woocommerce_shop_loop_item_title','woocommerce_template_loop_product_title',10);
-
-// Add custom product title function with custom class
-add_action('woocommerce_shop_loop_item_title','devollic_add_product_title_class',10);
-
-function devollic_add_product_title_class(){
-   echo " <h3 class='product-card__meta_title'>".get_the_title()."</h3>";
+if(class_exists('WooCommerce')){
+   require get_template_directory().'/inc/woocommerce/woocommerce-functions.php';
 }
--- inc/woocommerce/woocommerce-functions.php
+++ inc/woocommerce/woocommerce-functions.php
@@ -1,4 +1,5 @@
 <?php 
+global $product;
 
 // Remove default WooCommerce product title function
 remove_action('woocommerce_shop_loop_item_title','woocommerce_template_loop_product_title',10);
@@ -8,4 +9,16 @@ add_action('woocommerce_shop_loop_item_title','devollic_add_product_title_class'
 
 function devollic_add_product_title_class(){
    echo " <h3 class='product-card__meta_title'>".get_the_title()."</h3>";
+}
+
+// Remove ratings
+remove_action("woocommerce_after_shop_loop_item_title","woocommerce_template_loop_rating",5);
+
+// Add custom product price
+add_action('woocommerce_shop_loop_item_title','devollic_add_product_price_class',10);
+
+function devollic_add_product_price_class(){
+   if($price_html = $product -> get_price_html()): ?>
+      <div class='product-card__meta_price'><?php echo $price_html?></div>
+   <?php endif;
 }
