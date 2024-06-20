7e6976c67862931f280089975b835363d301ab63
mdrobin45 <robin4500bd@gmail.com>
2024-06-20 20:24:17 +0600

    modified:   functions.php 	renamed:    inc/woocommerce/functions.php -> inc/woocommerce/woocommerce-functions.php

--- functions.php
+++ functions.php
@@ -187,3 +187,9 @@ remove_action('woocommerce_before_main_content','woocommerce_breadcrumb',20);
 remove_action('woocommerce_shop_loop_header','woocommerce_product_taxonomy_archive_header',10);
 remove_action('woocommerce_before_shop_loop','woocommerce_result_count',20);
 remove_action('woocommerce_before_shop_loop','woocommerce_catalog_ordering',30);
+
+
+// Path: inc/woocommerce/woocommerce-functions.php
+if(class_exists('WooCommerce')){
+   include get_template_directory().'/inc/woocommerce/woocommerce-functions.php';
+}
