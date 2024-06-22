a2e5bbaa312e1e7e6ce5169431076e60a388f9bf
Robin Rana <robin4500bd@gmail.com>
2024-06-22 03:13:12 -0700

    update button

--- inc/woocommerce/woocommerce-functions.php
+++ inc/woocommerce/woocommerce-functions.php
@@ -105,5 +105,7 @@ function devollic_product_cart_meta_buttons(){
 
    $product_permalink = get_permalink($product->get_id());
    // display buttons
-   echo "<a data-product_id=".esc_attr($product->get_id())." data-product_sku=".esc_attr($product->get_sku())." aria-label=".esc_attr($product->get_name())." class='product_card__meta_button' href=".esc_url($product->add_to_cart_url()).">".esc_html('Add to Cart')."</a>";
+   echo "<div>";
+      echo "<a data-product_id=".esc_attr($product->get_id())." data-product_sku=".esc_attr($product->get_sku())." aria-label=".esc_attr($product->get_name())." class='product_card__meta_button' href=".esc_url($product->add_to_cart_url()).">".esc_html('Add to Cart')."</a>";
+   echo "</div>";
 }
