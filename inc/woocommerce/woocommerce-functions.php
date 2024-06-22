6e6965e318be2d91ee46c10a4b15553cfbf440e1
Robin Rana <robin4500bd@gmail.com>
2024-06-22 03:12:23 -0700

    Remove wrapper div

--- inc/woocommerce/woocommerce-functions.php
+++ inc/woocommerce/woocommerce-functions.php
@@ -105,9 +105,5 @@ function devollic_product_cart_meta_buttons(){
 
    $product_permalink = get_permalink($product->get_id());
    // display buttons
-   echo "<div class='product_card__meta_button_wrapper'>";
-      echo "<div>";
-         echo "<a data-product_id=".esc_attr($product->get_id())." data-product_sku=".esc_attr($product->get_sku())." aria-label=".esc_attr($product->get_name())." class='product_card__meta_button' href=".esc_url($product->add_to_cart_url()).">".esc_html('Add to Cart')."</a>";
-      echo "</div>";
-   echo "</div>";
+   echo "<a data-product_id=".esc_attr($product->get_id())." data-product_sku=".esc_attr($product->get_sku())." aria-label=".esc_attr($product->get_name())." class='product_card__meta_button' href=".esc_url($product->add_to_cart_url()).">".esc_html('Add to Cart')."</a>";
 }
