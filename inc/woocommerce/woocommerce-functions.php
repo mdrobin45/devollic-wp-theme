5aaa36678efb3d373985e8a959c5d3e72afbf976
Robin Rana <robin4500bd@gmail.com>
2024-06-24 22:48:52 +0600

    remove title

--- inc/woocommerce/woocommerce-functions.php
+++ inc/woocommerce/woocommerce-functions.php
@@ -11,7 +11,6 @@ add_action('woocommerce_shop_loop_item_title', 'devollic_add_product_title_class
  */
 function devollic_add_product_title_class()
 {
-   echo "HEllo";
     echo "<h3 class='product-card__meta_title'>" . get_the_title() . "</h3>";
 }
 
