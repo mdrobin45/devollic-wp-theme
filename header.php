d084a97422e6721eb4f5133b9c9d79d124dde354
Robin Rana <robin4500bd@gmail.com>
2024-07-12 18:34:56 +0600

    resolved header issue

--- header.php
+++ header.php
@@ -155,10 +155,8 @@
                         >Admin</span
                      >
                   </div>
-                  <button type="button" class="hover-text-danger">
-                     <iconify-icon
-                        icon="radix-icons:cross-1"
-                        class="icon text-xl"></iconify-icon>
+                  <button type="button" class="hover-text-danger header-user-dropdown-close-btn">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="2rem" height="2rem" viewBox="0 0 24 24"><path fill="none" stroke="black" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M6.758 17.243L12.001 12m5.243-5.243L12 12m0 0L6.758 6.757M12.001 12l5.243 5.243"/></svg>
                   </button>
                </div>
                <ul class="to-top-list">
