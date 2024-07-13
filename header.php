575213911176e8d74e7f95c45574f4e1f0fa3a25
Robin Rana <robin4500bd@gmail.com>
2024-07-13 21:10:12 +0600

    resolved header cart issue

--- header.php
+++ header.php
@@ -30,101 +30,91 @@
          'walker' => new Custom_Walker_Nav_Menu(),
       ) )
       ?>
-      <!-- <ul class="NavMenu m-0 m-0">
-         <li><a href="#">Home</a></li>
-         <li><a href="#">About Us</a></li>
-         <li>
-            <a href="javascript:void(0)"
-               ><label for="droplist1" class="toggle"
-                  >Our Service</label
-               ></a
-            >
-            <input type="checkbox" id="droplist1" />
-            <ul class="p-0 m-0">
-               <li><a href="#">Service A</a></li>
-               <li><a href="#">Service B</a></li>
-               <li><a href="#">Service D</a></li>
-            </ul>
-         </li>
-         <li><a href="#">Our Gallery</a></li>
-         <li><a href="#">Contact Us</a></li>
-         <li><a href="#">Enquiry</a></li>
-      </ul> -->
       <div class="d-flex align-items-center">
          <!-- Cart items -->
-         <div class="dropdown">
-            <button
-               class="has-indicator devollic_nav_cart-icon"
-               type="button"
-               data-bs-toggle="dropdown">
-               <div class="navbar_cart-btn">
-                  <div class="cart-icon">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="2rem" height="2rem" viewBox="0 0 512 512"><circle cx="176" cy="416" r="16" fill="none" stroke="black" stroke-linecap="round" stroke-linejoin="round" stroke-width="32"/><circle cx="400" cy="416" r="16" fill="none" stroke="black" stroke-linecap="round" stroke-linejoin="round" stroke-width="32"/><path fill="none" stroke="black" stroke-linecap="round" stroke-linejoin="round" stroke-width="32" d="M48 80h64l48 272h256"/><path fill="none" stroke="black" stroke-linecap="round" stroke-linejoin="round" stroke-width="32" d="M160 288h249.44a8 8 0 0 0 7.85-6.43l28.8-144a8 8 0 0 0-7.85-9.57H128"/></svg>
-                  </div>
-                  <div class="navbar_cart-btn-cart-badge">7</div>
-               </div>
-            </button>
-
-            <div class="dropdown-menu to-top dropdown-menu-lg p-0">
-               <div
-                  class="m-16 py-12 px-16 radius-8 bg-primary-50 mb-16 d-flex align-items-center justify-content-between gap-2">
-                  <div>
-                     <h6
-                        class="text-lg text-primary-light fw-semibold mb-0">
-                        Cart
-                     </h6>
-                  </div>
-                  <span
-                     class="text-primary-600 fw-semibold text-lg w-40-px h-40-px rounded-circle bg-base d-flex justify-content-center align-items-center"
-                     >05</span
-                  >
-               </div>
-
-               <div class="max-h-400-px overflow-y-auto scroll-sm pe-4">
-                  <a
-                     href="javascript:void(0)"
-                     class="px-24 py-12 d-flex align-items-center gap-3 mb-2 justify-content-between bg-neutral-50">
-                     <div
-                        class="text-black hover-bg-transparent hover-text-primary d-flex align-items-center gap-3">
-                        <span
-                           class="w-44-px h-44-px bg-success-subtle text-success-main rounded-circle d-flex justify-content-center align-items-center flex-shrink-0">
-                           <img
-                              src="assets/images/notification/profile-1.png"
-                              alt="" />
-                        </span>
-                        <div>
-                           <h6 class="text-md fw-semibold">
-                              Ronald Richards
-                           </h6>
-                           <p
-                              class="mb-0 text-sm text-secondary-light text-w-200-px">
-                              <strong class="fw-semibold text-black"
-                                 >Category:</strong
-                              >
-                              Wordpress
-                           </p>
-                        </div>
+          <?php
+          if(is_user_logged_in()): ?>
+            <div class="dropdown">
+               <button
+                  class="has-indicator devollic_nav_cart-icon"
+                  type="button"
+                  data-bs-toggle="dropdown">
+                  <div class="navbar_cart-btn">
+                     <div class="cart-icon">
+                     <svg xmlns="http://www.w3.org/2000/svg" width="2rem" height="2rem" viewBox="0 0 512 512"><circle cx="176" cy="416" r="16" fill="none" stroke="black" stroke-linecap="round" stroke-linejoin="round" stroke-width="32"/><circle cx="400" cy="416" r="16" fill="none" stroke="black" stroke-linecap="round" stroke-linejoin="round" stroke-width="32"/><path fill="none" stroke="black" stroke-linecap="round" stroke-linejoin="round" stroke-width="32" d="M48 80h64l48 272h256"/><path fill="none" stroke="black" stroke-linecap="round" stroke-linejoin="round" stroke-width="32" d="M160 288h249.44a8 8 0 0 0 7.85-6.43l28.8-144a8 8 0 0 0-7.85-9.57H128"/></svg>
                      </div>
-                     <p
-                        class="m-0 text-lg fw-semibold text-black text-secondary-light flex-shrink-0">
-                        $20
-                     </p>
-                  </a>
-               </div>
-               <div class="text-center cart-dropdown-footer">
-                  <a
-                     href="javascript:void(0)"
-                     class="text-white fw-semibold text-md py-12 px-16 d-block"
-                     >Proceed to Checkout</a
-                  >
+                     <div class="navbar_cart-btn-cart-badge"><?php echo WC()->cart->get_cart_contents_count(); ?></div>
+                  </div>
+               </button>
+
+               <div class="dropdown-menu to-top dropdown-menu-lg p-0">
+                  <div
+                     class="m-16 py-12 px-16 radius-8 bg-primary-50 mb-16 d-flex align-items-center justify-content-between gap-2">
+                     <div>
+                        <h6
+                           class="text-lg text-primary-light fw-semibold mb-0">
+                           Total
+                        </h6>
+                     </div>
+                     <span
+                        class="align-items-center bg-base d-flex fw-semibold h-40-px justify-content-center p-3 rounded-5 text-lg text-primary-600"
+                        ><?php wc_cart_totals_subtotal_html(); ?></span
+                     >
+                  </div>
+
+                  <div class="max-h-400-px overflow-y-auto scroll-sm pe-4">
+                     <a
+                        href="javascript:void(0)"
+                        class="px-24 py-12 d-flex align-items-center gap-3 mb-2 justify-content-between bg-neutral-50">
+                        <div
+                           class="text-black hover-bg-transparent hover-text-primary d-flex align-items-center gap-3">
+                           <span
+                              class="w-44-px h-44-px bg-success-subtle text-success-main rounded-circle d-flex justify-content-center align-items-center flex-shrink-0">
+                              <img
+                                 src="assets/images/notification/profile-1.png"
+                                 alt="" />
+                           </span>
+                           <div>
+                              <h6 class="text-md fw-semibold">
+                                 Ronald Richards
+                              </h6>
+                              <p
+                                 class="mb-0 text-sm text-secondary-light text-w-200-px">
+                                 <strong class="fw-semibold text-black"
+                                    >Category:</strong
+                                 >
+                                 Wordpress
+                              </p>
+                           </div>
+                        </div>
+                        <p
+                           class="m-0 text-lg fw-semibold text-black text-secondary-light flex-shrink-0">
+                           $20
+                        </p>
+                     </a>
+                  </div>
+                  <div class="text-center cart-dropdown-footer">
+                     <a
+                        href="javascript:void(0)"
+                        class="text-white fw-semibold text-md py-12 px-16 d-block"
+                        >Proceed to Checkout</a
+                     >
+                  </div>
                </div>
             </div>
-         </div>
-         <!-- Login Button -->
+         <?php endif; ?>
+
+         <!-- Login button -->
+         <?php
+          if(!is_user_logged_in()): ?>
          <a class="header-login-btn" href="#">Login</a>
+         <?php endif; ?>
+         
 
          <!-- User dropdown -->
-         <div class="dropdown">
+         <?php
+          if(is_user_logged_in()): ?>
+            <div class="dropdown">
             <button
                class="header-user-dropdown-btn"
                type="button"
@@ -177,6 +167,8 @@
                </ul>
             </div>
          </div>
+         <?php endif; ?>
+         
       </div>
    </nav>
    </header>
