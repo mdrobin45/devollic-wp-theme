aa6df5306f5d1029a6b1e12a55f16a1e9064b6bc
Robin Rana <robin4500bd@gmail.com>
2024-07-17 11:56:56 +0600

    configure logout and card

--- header.php
+++ header.php
@@ -149,20 +149,33 @@
          <!-- Login button -->
          <?php
           if(!is_user_logged_in()): ?>
-         <a class="header-login-btn" href="#">Login</a>
+         <a class="header-login-btn" href="<?php echo site_url('/login')?>">Login</a>
          <?php endif; ?>
          
 
          <!-- User dropdown -->
          <?php
           if(is_user_logged_in()): ?>
+          <?php
+               // Get the current user's ID
+                  $user_id = get_current_user_id();
+
+                  // Get the avatar for the user
+                  $avatar_url = get_avatar_url($user_id); 
+                  
+                  // Get current user data
+                  $current_user = wp_get_current_user();
+
+                  $user_name = $current_user->display_name;
+                  $user_email = $current_user->user_email;
+               ?>
             <div class="dropdown">
             <button
                class="header-user-dropdown-btn"
                type="button"
                data-bs-toggle="dropdown">
                <img
-                  src="https://wowdash.wowtheme7.com/demo/assets/images/user.png"
+                  src="<?php echo $avatar_url; ?>"
                   alt="image"
                   class="w-40-px h-40-px object-fit-cover rounded-circle" />
             </button>
@@ -172,10 +185,10 @@
                   <div>
                      <h6
                         class="text-lg text-primary-light fw-semibold mb-2">
-                        Shaidul Islam
+                        <?php echo $user_name; ?>
                      </h6>
                      <span class="text-secondary-light fw-medium text-sm"
-                        >Admin</span
+                        ><?php echo $user_email?></span
                      >
                   </div>
                   <button type="button" class="hover-text-danger header-user-dropdown-close-btn">
@@ -186,14 +199,14 @@
                   <li>
                      <a
                         class="dropdown-item text-black px-0 py-8 hover-bg-transparent hover-text-primary d-flex align-items-center gap-3"
-                        href="view-profile.html">
+                        href="<?php echo site_url('/my-account/downloads'); ?>">
                         <svg xmlns="http://www.w3.org/2000/svg" width="1.5rem" height="1.5rem" viewBox="0 0 24 24"><path fill="black" d="M9 1v6H5l7 7l7-7h-4V1zM5 16v2h14v-2zm0 4v2h14v-2z"/></svg>
                         Downloads</a>
                   </li>
                   <li>
                      <a
                         class="dropdown-item text-black px-0 py-8 hover-bg-transparent hover-text-primary d-flex align-items-center gap-3"
-                        href="email.html">
+                        href="<?php echo site_url('/my-account/orders')?>">
                         <svg xmlns="http://www.w3.org/2000/svg" width="1.5rem" height="1.5rem" viewBox="0 0 24 24"><path fill="black" d="m23.5 17l-5 5l-3.5-3.5l1.5-1.5l2 2l3.5-3.5zM6 2a2 2 0 0 0-2 2v16c0 1.11.89 2 2 2h7.81c-.36-.62-.61-1.3-.73-2H6V4h7v5h5v4.08c.33-.05.67-.08 1-.08c.34 0 .67.03 1 .08V8l-6-6M8 12v2h8v-2m-8 4v2h5v-2Z"/></svg>
                         Invoice</a
                      >
@@ -201,7 +214,7 @@
                   <li>
                      <a
                         class="dropdown-item text-black px-0 py-8 hover-bg-transparent hover-text-danger d-flex align-items-center gap-3"
-                        href="javascript:void(0)">
+                        href="<?php echo wp_logout_url(site_url()); ?>">
                         <svg xmlns="http://www.w3.org/2000/svg" width="1.5rem" height="1.5rem" viewBox="0 0 24 24"><path fill="black" d="M4 12a.5.5 0 0 0 .5.5h8.793l-2.647 2.646a.5.5 0 1 0 .707.708l3.5-3.5a.5.5 0 0 0 0-.707l-3.5-3.5a.5.5 0 0 0-.707.707l2.647 2.646H4.5a.5.5 0 0 0-.5.5M17.5 2h-11A2.502 2.502 0 0 0 4 4.5v4a.5.5 0 0 0 1 0v-4A1.5 1.5 0 0 1 6.5 3h11A1.5 1.5 0 0 1 19 4.5v15a1.5 1.5 0 0 1-1.5 1.5h-11A1.5 1.5 0 0 1 5 19.5v-4a.5.5 0 0 0-1 0v4A2.502 2.502 0 0 0 6.5 22h11a2.502 2.502 0 0 0 2.5-2.5v-15A2.502 2.502 0 0 0 17.5 2"/></svg>
                         Log Out</a
                      >
